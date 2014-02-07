5ab168639b4743e990037b46b5fd15e95c4fe222
celliot <christopher.m.elliot@gmail.com>
2014-02-07 14:07:14 +1100

    register post to PHP

---
+++ register.php
@@ -0,0 +1,19 @@
+<?php
+	var_dump($_POST);
+/*
+	$_POST['firstname'];
+	$_POST['surname'];
+	$_POST['username'];
+	$_POST['pwd'];
+	$_POST['dob'];//date of birth
+	$_POST['mob'];//month of birth
+	$_POST['yob'];//year of birth
+	// date would be dd / m / YY
+	$_POST['genderMale'];
+	$_POST{'genderFemale'];
+	$_POST['email'];
+	$_POST['address'];
+	$_POST['membershipType'];
+	$_POST['memberDuration'];
+*/
+?>
