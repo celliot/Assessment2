2b2e48bfcb214063239f85e576953ab090c7c412
celliot <christopher.m.elliot@gmail.com>
2014-02-07 12:25:18 +1100

    getting started

---
+++ Scripts/main.js
@@ -0,0 +1,35 @@
+function loadHome() {
+		document.getElementById("currentFrame").src = 'Main.html';
+		return false;
+	}
+	
+	function loadCastCrew(){
+		document.getElementById("currentFrame").src = 'castCrew.html';
+		return false;
+	}
+	
+	function loadGallery() {
+		document.getElementById("currentFrame").src = 'gallery.html';
+		return false;
+	}
+	
+	function loadShopping() {
+		document.getElementById("currentFrame").src = 'shopping.html';
+		return false;
+	}
+	
+	function loadVideos(){
+		document.getElementById("currentFrame").src = 'videos.html';
+		return false;
+	}
+	
+	function loadHallofFame(){
+		document.getElementById("currentFrame").src = 'hallofFame.html';
+		return false;
+	}
+	
+	function loadSignIn(){
+		document.getElementById("currentFrame").src = 'login.html';
+		return false;
+	}
+	
---
+++ Scripts/trailers.js
@@ -0,0 +1,24 @@
+function loadTrailer1() {
+		document.getElementById("video").src = "http://www.youtube.com/embed/fnaojlfdUbs?feature=player_embedded";
+		return false;
+	}
+	function loadTrailer2() {
+		document.getElementById("video").src = "http://www.youtube.com/embed/mbOEknbi4gQ?feature=player_embedded";
+		return false;
+	}
+	function loadTrailer3() {
+		document.getElementById("video").src = "http://www.youtube.com/embed/lfflhfn1W-o?feature=player_embedded";
+		return false;
+	}
+	function loadTrailer4() {
+		document.getElementById("video").src = "http://www.youtube.com/embed/JqdZGrgZPqI?feature=player_embedded";
+		return false;
+	}
+	function loadTrailer5() {
+		document.getElementById("video").src = "http://www.youtube.com/embed/1EljRMcdk7g?feature=player_embedded";
+		return false;
+	}
+	function loadTrailer6() {
+		document.getElementById("video").src = "http://www.youtube.com/embed/njbObb1OOC8?feature=player_embedded";
+		return false;
+	}
